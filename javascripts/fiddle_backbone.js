c3fde3434148bd7b8b5cb96c6633e85c67866cf6
Jake Feasel <jfeasel@gmail.com>
2012-03-05 14:51:26 -0900

    Handling cases when unexpected results returned from loadContent

--- javascripts/fiddle_backbone.js
+++ javascripts/fiddle_backbone.js
@@ -26,24 +26,26 @@ $(function () {
 			this.DBType(db_type_id);
 			$("body").block({ message: "Loading..."});
 			$.getJSON("index.cfm/fiddles/loadContent", {fragment: frag}, function (resp) {
-				window.schemaDef.set({
-					"short_code": resp["short_code"],
-					"ddl": resp["ddl"],
-					"ready": true,
-					"valid": true,
-					"errorMessage": ""
-				});
-				window.schemaDef.trigger("reloaded");
-				window.schemaDef.trigger("built");				
-				window.query.reset();
-				window.query.trigger("reloaded");		
-				
-				window.myFiddleHistory.insert(new UsedFiddle({
-					"fragment": frag,
-					"full_name": window.dbTypes.getSelectedType().get("full_name"),
-					"ddl": resp["ddl"] 
-				}));
-				
+				if (resp["short_code"])
+				{
+					window.schemaDef.set({
+						"short_code": resp["short_code"],
+						"ddl": resp["ddl"],
+						"ready": true,
+						"valid": true,
+						"errorMessage": ""
+					});
+					window.schemaDef.trigger("reloaded");
+					window.schemaDef.trigger("built");				
+					window.query.reset();
+					window.schemaDef.trigger("reloaded");
+					
+					window.myFiddleHistory.insert(new UsedFiddle({
+						"fragment": frag,
+						"full_name": window.dbTypes.getSelectedType().get("full_name"),
+						"ddl": resp["ddl"] 
+					}));
+				}
 				$("body").unblock();
 						
 			});
@@ -57,31 +59,37 @@ $(function () {
 
 			$("body").block({ message: "Loading..."});
 			$.getJSON("index.cfm/fiddles/loadContent", {fragment: frag}, function (resp) {
-				window.schemaDef.set({
-					"short_code": resp["short_code"],
-					"ddl": resp["ddl"],
-					"ready": true,
-					"valid": true,
-					"errorMessage": ""
-				});
-				window.schemaDef.trigger("reloaded");
-				
-				window.query.set({
-					"id": query_id,
-					"sql": resp["sql"],
-					"sets": resp["sets"]
-				});
-				window.query.trigger("reloaded");
 
-				window.myFiddleHistory.insert(new UsedFiddle({
-					"fragment": frag,
-					"full_name": window.dbTypes.getSelectedType().get("full_name"),
-					"ddl": resp["ddl"],
-					"sql": resp["sql"] 
-				}));
-						
+				if (resp["short_code"])
+				{
+
+					window.schemaDef.set({
+						"short_code": resp["short_code"],
+						"ddl": resp["ddl"],
+						"ready": true,
+						"valid": true,
+						"errorMessage": ""
+					});
+					window.schemaDef.trigger("reloaded");
+					
+					if (resp["sql"])
+					{
+						window.query.set({
+							"id": query_id,
+							"sql": resp["sql"],
+							"sets": resp["sets"]
+						});
+						window.query.trigger("reloaded");
+		
+						window.myFiddleHistory.insert(new UsedFiddle({
+							"fragment": frag,
+							"full_name": window.dbTypes.getSelectedType().get("full_name"),
+							"ddl": resp["ddl"],
+							"sql": resp["sql"] 
+						}));
+					}							
 				
-				
+				}
 				$("body").unblock();
 			});
 
