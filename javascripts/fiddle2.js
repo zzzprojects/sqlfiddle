60c1a9deb255cb8555579f028324e34aa9cf2585
Jake Feasel <jfeasel@gmail.com>
2012-03-08 20:56:30 -0900

    Using vkbeautify to format the execution plans when they are xml

--- javascripts/fiddle2.js
+++ javascripts/fiddle2.js
@@ -1,34 +1,44 @@
-	
-	$.blockUI.defaults.overlayCSS.cursor = 'auto';
-	$.blockUI.defaults.css.cursor = 'auto';
 
 $(function () {
+
+	$("#textToDDLModal .btn").click(function (e){
+		e.preventDefault();
+
+		var builder = new ddl_builder({
+				tableName: $("#tableName").val()
+			})
+			.setupForDBType(window.dbTypes.getSelectedType().get("simple_name"));
 		
-
-		$("#textToDDLModal .btn").click(function (e){
-			e.preventDefault();
-
-			var builder = new ddl_builder({
-					tableName: $("#tableName").val()
-				})
-				.setupForDBType(window.dbTypes.getSelectedType().get("simple_name"));
-			
-			var ddl = builder.parse($("#raw").val());
-			
-			$("#parseResults").text(ddl);
-			
-			if ($(this).attr('id') == 'appendDDL')
-			{
-				window.schemaDef.set("ddl", window.schemaDef.get("ddl") + "\n\n" + ddl);
-				window.schemaDef.trigger("reloaded");
-				$('#textToDDLModal').modal('hide');
-			}
-		});
+		var ddl = builder.parse($("#raw").val());
 		
+		$("#parseResults").text(ddl);
+		
+		if ($(this).attr('id') == 'appendDDL')
+		{
+			window.schemaDef.set("ddl", window.schemaDef.get("ddl") + "\n\n" + ddl);
+			window.schemaDef.trigger("reloaded");
+			$('#textToDDLModal').modal('hide');
+		}
+	});
+	
 
-		$(window).bind('resize', resizeLayout);		
-		setTimeout(resizeLayout, 1);
+	$(window).bind('resize', resizeLayout);		
+	setTimeout(resizeLayout, 1);
 });
+
+$.blockUI.defaults.overlayCSS.cursor = 'auto';
+$.blockUI.defaults.css.cursor = 'auto';
+
+
+Handlebars.registerHelper("xmlPretty", function() {
+	try {
+		$.parseXML(this);
+		return new Handlebars.SafeString(vkbeautify.xml(this));
+	}
+	catch (e) {
+		return new Handlebars.SafeString(this);
+	}
+});		
 
 
 
