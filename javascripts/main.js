9b79feb32901f8689b0111b0355bc604c87db21b
jakefeasel <jfeasel@gmail.com>
2012-07-11 13:22:18 -0700

    Apparently google analytics doesn't work within the complete handler for requirejs

--- javascripts/main.js
+++ javascripts/main.js
@@ -31,15 +31,4 @@ require([	'jquery','underscore','json2','codemirror','bootstrap-tooltip','sqlite
 			'bootstrap-collapse','bootstrap-dropdown','bootstrap-modal','bootstrap-popover','fiddle2'
 		], function($) {
 	
-	  var _gaq = _gaq || [];
-	  _gaq.push(['_setAccount', 'UA-28576776-1']);
-	  _gaq.push(['_setDomainName', 'sqlfiddle.com']);
-	  _gaq.push(['_trackPageview']);
-	
-	  (function() {
-	    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-	    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-	    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-	  })();
-	
 });
