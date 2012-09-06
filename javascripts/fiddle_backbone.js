dd7f3c8877fb78aec3e059076ffae58769eb95bf
Jake Feasel <jfeasel@gmail.com>
2012-09-05 20:15:11 -0700

    New logic to show links when valid http URLS are include in the response body

--- javascripts/fiddle_backbone.js
+++ javascripts/fiddle_backbone.js
@@ -1143,12 +1143,18 @@ $(function () {
 
     
 	Handlebars.registerHelper("result_display", function(value) {
+		// thanks to John Gruber for this regexp http://daringfireball.net/2010/07/improved_regex_for_matching_urls
+		// also to "Searls" for his port to JS https://gist.github.com/1033143
+		var urlRegexp = /\b((?:https?:\/\/|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?гхрсту]))/ig;
+		
 		if ($.isPlainObject(value))
 			return JSON.stringify(value);
 		else if (value == null)
 			return "(null)";
 		else if (value === false)
 			return "false";
+		else if (value.match(urlRegexp) && Handlebars.Utils.escapeExpression(value) == value)
+			return new Handlebars.SafeString(value.replace(urlRegexp, "<a href='$1' target='_new'>$1</a>"));
 		else
 			return value;
 	});
